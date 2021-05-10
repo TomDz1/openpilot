903262c8d61ce6d41b959d971c4a4093dd03aa3e
kegman <8837066+kegman@users.noreply.github.com>
2021-05-09 20:12:41 -0400

    log.txt

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -187,7 +187,8 @@ function launch {
 
   # start manager
   cd selfdrive/manager
-  ./build.py && ./manager.py > log.txt
+  ./build.py
+  ./manager.py > log.txt
 
   # if broken, keep on screen error
   while true; do sleep 1; done
